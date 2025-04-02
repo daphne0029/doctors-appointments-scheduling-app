8e4da0dc7077636a0d4e8d4c712ecff0d7a0d4b5
Daphne Chiang <daphne0029@hotmail.com>
2025-04-02 09:48:40 -0700

    Added patient endpoints

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PatientController extends Controller
+{
+    /**
+     * Create a patient
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(),[
+            'name'  => 'required|string|max:255',
+            'email' => 'required|string|email|unique:patients,email',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation failed',
+                'errors'  => $validator->errors(),
+            ], 422);
+        }
+
+        $patient = Patient::create($validatedData);
+
+        return response()->json($patient, 201);
+    }
+
+    /**
+     * Retrieve a paitent by ID.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show($id)
+    {
+        $patient = Patient::find($id);
+
+        if (!$patient) {
+            return response()->json(['message' => 'Patient not found'], 404);
+        }
+
+        return response()->json($patient);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\PatientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,6 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/doctors', [DoctorController::class, 'index']);
 Route::get('/doctors/{id}', [DoctorController::class, 'show']);
+
+Route::post('/patients', [PatientController::class, 'store']);
+Route::get('/patients/{id}', [PatientController::class, 'show']);
